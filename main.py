73f5f24e722d538784e6753d55e46fd44531acab
thormisepriyanka <thormisepriyanka0704@gmail.com>
2024-10-09 13:45:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# a ="Hello"
+# b = "Priyanka"
+# print(a+b)
+
+# a = 3
+# b= 5
+# print(a+b)
+
+# a = "3"
+# b= "5"
+# print(a+b)
+
+# var = "533+10j"
+# var = complex(var)   
+# print(type(var))
+# print(var)
+
+
+
+# a = "Teen"
+# b= "Paach"
+# print(a,b, end="/n")
+
+# x= "She is good girl"
+# y= "How?"
+# # print(f"How is she?-->{x}")
+# print("How is she?-->{}".format(x,y))
+
+
+# name = input("Entr your name:")
+# print(f"Hi{name}, Good Morning!")
+# print("Hi "+name+", Good Morning!")
+
+# lst = [1,"abhi",33.43,"20j"]
+# lst.remove("abhi")
+# print(lst)
+
+
+
+# lst = [1,"abhi",33.43,"20j"]
+# # lst.remove()
+# lst.pop(1)
+# print(lst)
+
+x = "Priyanka"
+
+print(x[2])
